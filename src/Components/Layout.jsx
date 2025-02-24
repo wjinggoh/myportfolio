0c3e3ee5ea4d2aa283c1fbeb818260217f448983
wjinggoh <wjingggoh15@gmail.com>
2025-02-24 21:25:40 +0800

    oh

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,1 +1,15 @@
 
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
